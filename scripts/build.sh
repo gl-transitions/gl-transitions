7782394ee38f989e6637b918f16275027bcf88ce
Gaëtan Renaudeau <renaudeau.gaetan@gmail.com>
2017-05-21 10:01:02 +0200

    release folder

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,6 +1,16 @@
 cd $(dirname $0)/..
+set -e
 
-rm -f transitions.json &&
-gl-transition-transform -d transitions -o transitions.json &&
-echo "global.GLTransitions=" | cat - transitions.json > ./gl-transitions.js &&
-echo "module.exports=" | cat - transitions.json > ./index.js
+remoteVersion=`npm show gl-transitions version`
+
+rm -rf release/
+cp -R scripts/release-skeleton release
+cd release
+npm version $remoteVersion --no-git-tag-version
+npm version minor --no-git-tag-version
+cd -
+
+gl-transition-transform -d transitions -o release/transitions.json
+cd release
+echo "window.GLTransitions=" | cat - transitions.json > gl-transitions.js
+echo "module.exports=" | cat - transitions.json > index.js
