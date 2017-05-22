80ca5400b6b68cb170eac84669e5c5bd1535c231
Gaëtan Renaudeau <renaudeau.gaetan@gmail.com>
2017-05-22 21:00:26 +0200

    improve gif-it loop

--- scripts/gif-it.sh
+++ scripts/gif-it.sh
@@ -12,11 +12,13 @@ gif="/tmp/gl-transition.gif"
 
 rm -rf $tmpimgs $palette $gif
 
-gl-transition-render -t $tname --from scripts/in.jpg --to scripts/out.jpg -w 512 -h 400 -f 50 -o $tmpimgs
+gl-transition-render -t $tname -o $tmpimgs \
+  -i scripts/1.jpg,scripts/2.jpg,scripts/3.jpg,scripts/1.jpg \
+  -f 50 -d 12 -w 512 -h 400
 
 filters="scale=256:-1:flags=lanczos"
-ffmpeg -v fatal -framerate 20 -i $tmpimgs/%d.png -vf "$filters,palettegen" -y $palette
-ffmpeg -v fatal -framerate 20 -i $tmpimgs/%d.png -i $palette -lavfi "$filters [x]; [x][1:v] paletteuse" -y $gif
+ffmpeg -v fatal -framerate 30 -i $tmpimgs/%d.png -vf "$filters,palettegen" -y $palette
+ffmpeg -v fatal -framerate 30 -i $tmpimgs/%d.png -i $palette -lavfi "$filters [x]; [x][1:v] paletteuse" -y $gif
 
 curl -sS -H "Authorization: Client-ID $imgurkey" -H 'Expect: ' -F "image=@$gif" https://api.imgur.com/3/image |
 json data.link
