bd967e5d547901a54c323982defe21b22bcadb80
Gaëtan Renaudeau <renaudeau.gaetan@gmail.com>
2017-05-29 10:53:58 +0200

    trying something

--- transitions/pixelize.glsl
+++ transitions/pixelize.glsl
@@ -7,8 +7,8 @@ uniform float steps /* = 50 */; // zero disable the stepping
 
 vec4 transition(vec2 uv) {
   float dist = min(progress, 1.0 - progress);
-  dist = mix(ceil(dist * steps) / steps, dist, step(steps, 0.0));
+  dist = mix(ceil(dist * steps) / steps, dist, step(steps, 0.001));
   vec2 squareSize = 2.0 * dist / squaresMin;
-  vec2 p = dist>0.0 ? (floor(uv / squareSize - 0.5) + 0.5) * squareSize : uv;
+  vec2 p = mix((floor(uv / squareSize - 0.5) + 0.5) * squareSize, uv, step(dist, 0.001));
   return mix(getFromColor(p), getToColor(p), progress);
 }
