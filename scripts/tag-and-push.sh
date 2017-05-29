bd902c7663ece26f1b0897bb7f5f39030a6fdf95
Gaëtan Renaudeau <renaudeau.gaetan@gmail.com>
2017-05-29 08:58:41 +0200

    gltransitions/gl-transitions -> gl-transitions/gl-transitions org

--- scripts/tag-and-push.sh
+++ scripts/tag-and-push.sh
@@ -3,4 +3,4 @@ set -e
 
 GIT_TAG=v`npm show gl-transitions version`
 git tag $GIT_TAG
-git push https://$GITHUB_TOKEN@github.com/gltransitions/gl-transitions $GIT_TAG
+git push https://$GITHUB_TOKEN@github.com/gl-transitions/gl-transitions $GIT_TAG
