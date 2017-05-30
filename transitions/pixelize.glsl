36edbaca51713396cb6c4c81417f159a307c3302
Gaëtan Renaudeau <renaudeau.gaetan@gmail.com>
2017-05-30 12:32:59 +0200

    prefer ivec2 & int

--- transitions/pixelize.glsl
+++ transitions/pixelize.glsl
@@ -4,14 +4,13 @@
 
 // FIXME something was kinda better in this one: https://gist.github.com/corporateshark/21d2fdd24c706952dc8c
 
-uniform vec2 squaresMin/* = vec2(20.0) */; // minimum number of squares (when the effect is at its higher level)
-uniform float steps /* = 50 */; // zero disable the stepping
+uniform ivec2 squaresMin/* = ivec2(20) */; // minimum number of squares (when the effect is at its higher level)
+uniform int steps /* = 50 */; // zero disable the stepping
 
 vec4 transition(vec2 uv) {
   float dist = min(progress, 1.0 - progress);
-  dist = steps>0.0 ? ceil(dist * steps) / steps : dist;
-  vec2 squareSize = 2.0 * dist / squaresMin;
+  dist = steps>0 ? ceil(dist * float(steps)) / float(steps) : dist;
+  vec2 squareSize = 2.0 * dist / vec2(squaresMin);
   vec2 p = dist>0.0 ? (floor(uv / squareSize - 0.5) + 0.5) * squareSize : uv;
   return mix(getFromColor(p), getToColor(p), progress);
 }
-
