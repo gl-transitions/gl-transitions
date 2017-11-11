b811fd7e1d932c01c378534631ba19e05875b4af
Gaëtan Renaudeau <renaudeau.gaetan@gmail.com>
2017-11-11 08:50:40 +0100

    Update Bounce.glsl

--- transitions/Bounce.glsl
+++ transitions/Bounce.glsl
@@ -1,31 +1,29 @@
 // Author: Adrian Purser
 // License: MIT
 
+uniform vec4 shadow_colour; // = vec4(0.,0.,0.,.6)
+uniform float shadow_height; // = 0.075
+uniform float bounces; // = 3.0
+
+const float PI = 3.14159265358;
+
 vec4 transition (vec2 uv) {
-  float   time = progress;
-  float 	stime = sin(time * (3.14159265358/2.0));
-	float 	phase = time * 3.14159265358 * 3.0;
-	float 	shadow = 0.075;
-	vec4 	shadow_colour = vec4(0.0,0.0,0.0,0.6);
-	float y = (abs(cos(phase))) * (1.0-stime);
-	vec4 colour;
-	
-	if(progress == 1.0)
-	  colour = getToColor(uv);
-	else if(uv.y > y)
-	{
-		float d = uv.y - y;
-		if(d>shadow)
-			colour = getToColor(uv);
-		else
-		{
-			float a = ((d/shadow)*shadow_colour.a) + (1.0-shadow_colour.a);
-			colour = mix(shadow_colour,getToColor(uv),a);
-		}
-	}
-	else
-	{
-		colour = getFromColor(vec2(uv.x,uv.y+(1.0-y)));
-	}	
-  return colour;
+  float time = progress;
+  float stime = sin(time * PI / 2.);
+  float phase = time * PI * bounces;
+  float y = (abs(cos(phase))) * (1.0 - stime);
+  float d = uv.y - y;
+  return mix(
+    mix(
+      getToColor(uv),
+      shadow_colour,
+      step(d, shadow_height) * (1. - mix(
+        ((d / shadow_height) * shadow_colour.a) + (1.0 - shadow_colour.a),
+        1.0,
+        smoothstep(0.95, 1., progress) // fade-out the shadow at the end
+      ))
+    ),
+    getFromColor(vec2(uv.x, uv.y + (1.0 - y))),
+    step(d, 0.0)
+  );
 }
