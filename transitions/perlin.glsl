ea34f3efa7502a30f646a0719a54f4f88ec5dc5b
Rich Harris <richard.a.harris@gmail.com>
2018-01-23 21:35:10 -0500

    Update perlin.glsl

---
+++ transitions/perlin.glsl
@@ -0,0 +1,57 @@
+// Author: Rich Harris
+// License: MIT
+
+#ifdef GL_ES
+precision mediump float;
+#endif
+
+uniform float scale; // = 4.0
+uniform float smoothness; // = 0.01
+
+// 2D Random
+float random (in vec2 st) {
+    return fract(sin(dot(st.xy,
+                         vec2(12.9898,78.233)))
+                 * 43758.5453123);
+}
+
+// 2D Noise based on Morgan McGuire @morgan3d
+// https://www.shadertoy.com/view/4dS3Wd
+float noise (in vec2 st) {
+    vec2 i = floor(st);
+    vec2 f = fract(st);
+
+    // Four corners in 2D of a tile
+    float a = random(i);
+    float b = random(i + vec2(1.0, 0.0));
+    float c = random(i + vec2(0.0, 1.0));
+    float d = random(i + vec2(1.0, 1.0));
+
+    // Smooth Interpolation
+
+    // Cubic Hermine Curve.  Same as SmoothStep()
+    vec2 u = f*f*(3.0-2.0*f);
+    // u = smoothstep(0.,1.,f);
+
+    // Mix 4 coorners porcentages
+    return mix(a, b, u.x) +
+            (c - a)* u.y * (1.0 - u.x) +
+            (d - b) * u.x * u.y;
+}
+
+vec4 transition (vec2 uv) {
+  vec4 from = getFromColor(uv);
+  vec4 to = getToColor(uv);
+  float n = noise(uv * scale);
+  
+  float lower = max(n - smoothness, 0.0);
+  float higher = min(n + smoothness, 1.0);
+  
+  float q = (progress - lower) / (higher - lower);
+  
+  return mix(
+    from,
+    to,
+    q
+  );
+}
