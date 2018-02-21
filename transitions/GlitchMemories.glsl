5c3ce6ed120fe2ff6f9ea20203d8be450edd0167
Gunnar Roth <gunnar.roth@gmx.de>
2018-02-21 19:25:08 +0100

    fix invalid transition: getFromColor(uv) MUST render when progress=0.0

--- transitions/GlitchMemories.glsl
+++ transitions/GlitchMemories.glsl
@@ -4,7 +4,7 @@ vec4 transition(vec2 p) {
   vec2 block = floor(p.xy / vec2(16));
   vec2 uv_noise = block / vec2(64);
   uv_noise += floor(vec2(progress) * vec2(1200.0, 3500.0)) / vec2(64);
-  vec2 dist = (fract(uv_noise) - 0.5) * 0.3 *(1.0 -progress);
+  vec2 dist = progress > 0.0 ? (fract(uv_noise) - 0.5) * 0.3 *(1.0 -progress) : vec2(0.0);
   vec2 red = p + dist * 0.2;
   vec2 green = p + dist * .3;
   vec2 blue = p + dist * .5;
