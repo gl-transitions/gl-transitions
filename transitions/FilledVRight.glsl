a731efdcb10daa2565a6a33289c09cebd2dd5cb0
Ngo Xuan Huy <ngoxuanhuy96@gmail.com>
2018-03-28 10:06:59 +0700

    Add "V" transition
    Add "V" transition in 4 different directions

---
+++ transitions/FilledVRight.glsl
@@ -0,0 +1,64 @@
+// Author: huynx
+// License: MIT
+
+float check(vec2 p1, vec2 p2, vec2 p3)
+{
+  return (p1.x - p3.x) * (p2.y - p3.y) - (p2.x - p3.x) * (p1.y - p3.y);
+}
+
+bool PointInTriangle (vec2 pt, vec2 p1, vec2 p2, vec2 p3)
+{
+    bool b1, b2, b3;
+    b1 = check(pt, p1, p2) < 0.0;
+    b2 = check(pt, p2, p3) < 0.0;
+    b3 = check(pt, p3, p1) < 0.0;
+    return ((b1 == b2) && (b2 == b3));
+}
+
+bool in_left_triangle(vec2 p){
+  vec2 vertex1, vertex2, vertex3;
+  vertex1 = vec2(0.0+progress*2.0, 0.5);
+  vertex2 = vec2(0.0, 0.5-progress);
+  vertex3 = vec2(0.0, 0.5+progress);
+  if (PointInTriangle(p, vertex1, vertex2, vertex3))
+  {
+    return true;
+  }
+  return false;
+}
+
+float blur_edge(vec2 bot1, vec2 bot2, vec2 top, vec2 testPt)
+{
+  vec2 lineDir = bot1 - top;
+  vec2 perpDir = vec2(lineDir.y, -lineDir.x);
+  vec2 dirToPt1 = bot1 - testPt;
+  float dist1 = abs(dot(normalize(perpDir), dirToPt1));
+  
+  lineDir = bot2 - top;
+  perpDir = vec2(lineDir.y, -lineDir.x);
+  dirToPt1 = bot2 - testPt;
+  float min_dist = min(abs(dot(normalize(perpDir), dirToPt1)), dist1);
+  
+  if (min_dist < 0.005) {
+    return min_dist / 0.005;
+  }
+  else  {
+    return 1.0;
+  };
+}
+
+vec4 transition (vec2 uv) {
+  if (progress <= 0.01)
+  {
+    return getFromColor(uv);
+  }
+  if (in_left_triangle(uv))
+  {
+      return mix(
+        getFromColor(uv),
+        getToColor(uv),
+        blur_edge(vec2(0.0, 0.5+progress),vec2(0.0, 0.5-progress),vec2(0.0+progress*2.0, 0.5), uv)
+      );
+  }
+  return getFromColor(uv);
+}
