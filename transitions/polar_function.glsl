c3748d155672850a089b3dc90d65e8d88c1ba03a
jsaluja <jaspal.saluja@gmail.com>
2021-01-23 00:27:23 -0800

    Drop unused variable normalized

--- transitions/polar_function.glsl
+++ transitions/polar_function.glsl
@@ -8,7 +8,6 @@ uniform int segments; // = 5;
 vec4 transition (vec2 uv) {
   
   float angle = atan(uv.y - 0.5, uv.x - 0.5) - 0.5 * PI;
-  float normalized = (angle + 1.5 * PI) * (2.0 * PI);
   
   float radius = (cos(float(segments) * angle) + 4.0) / 4.0;
   float difference = length(uv - vec2(0.5, 0.5));
