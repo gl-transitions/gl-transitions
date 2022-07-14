dedb44f8b1b521a096fa492bcf2e40992450407a
Mark Craig <mrmcsoftware@users.noreply.github.com>
2022-07-14 01:17:36 -0400

    Create RotateScaleVanish.glsl
    Rotates the image out/in while scaling and fading out.

---
+++ transitions/RotateScaleVanish.glsl
@@ -0,0 +1,40 @@
+// Author: Mark Craig
+// mrmcsoftware on github and youtube ( http://www.youtube.com/MrMcSoftware )
+// License: MIT
+
+// RotateScaleVanish Transition by Mark Craig (Copyright © 2022)
+
+uniform bool FadeInSecond; // = true
+uniform bool ReverseEffect; // = false
+uniform bool ReverseRotation; // = false
+
+#define M_PI 3.14159265358979323846
+#define _TWOPI 6.283185307179586476925286766559
+
+vec4 transition(vec2 uv)
+{
+if (progress == 0.0) { return(getFromColor(uv)); }
+else if (progress == 1.0) { return(getToColor(uv)); }
+vec2 iResolution = vec2(ratio, 1.0);
+float t;
+if (ReverseEffect) { t = 1.0 - progress; } else { t = progress; }
+float theta = ReverseRotation ? _TWOPI * t : -_TWOPI * t;
+float c1 = cos(theta);
+float s1 = sin(theta);
+float rad = 1.0 - t;
+if (rad <= 0.0) { rad = 0.00001; }
+float xc1 = (uv.x - 0.5) * iResolution.x;
+float yc1 = (uv.y - 0.5) * iResolution.y;
+float xc2 = (xc1 * c1 - yc1 * s1) / rad;
+float yc2 = (xc1 * s1 + yc1 * c1) / rad;
+vec2 uv2 = vec2(xc2 + iResolution.x / 2.0, yc2 + iResolution.y / 2.0);
+vec4 col3;
+vec4 ColorTo = ReverseEffect ? getFromColor(uv) : getToColor(uv);
+if ((uv2.x >= 0.0) && (uv2.x <= iResolution.x) && (uv2.y >= 0.0) && (uv2.y <= iResolution.y))
+	{
+	uv2 /= iResolution;
+	col3 = ReverseEffect ? getToColor(uv2) : getFromColor(uv2);
+	}
+else { col3 = FadeInSecond ? vec4(0.0, 0.0, 0.0, 1.0) : ColorTo; }
+return((1.0 - t) * col3 + t * ColorTo); // could have used mix
+}
