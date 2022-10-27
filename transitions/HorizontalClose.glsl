18014c00ebd1570d0a4eb0d5e30989b90d9139fe
martyniukigor <83081473+martyniukigor@users.noreply.github.com>
2022-10-27 09:41:47 +0300

    Create HorizontalClose.glsl

---
+++ transitions/HorizontalClose.glsl
@@ -0,0 +1,13 @@
+// Author: martiniti
+// License: MIT
+
+vec4 transition (vec2 uv) {
+
+  float s = 2.0 - abs((uv.y - 0.5) / (progress - 1.0)) - 2.0 * progress;
+  
+  return mix(
+    getFromColor(uv),
+    getToColor(uv),
+    smoothstep(0.5, 0.0, s)
+  ); 
+}
