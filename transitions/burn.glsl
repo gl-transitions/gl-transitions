619038c38a43f9291291dd09e0fd81a4c674844f
Brad Edelman <brad@scenery.video>
2023-04-04 13:17:00 -0700

    fix 1 more

--- transitions/burn.glsl
+++ transitions/burn.glsl
@@ -1,10 +1,11 @@
 // author: gre
 // License: MIT
 uniform vec3 color /* = vec3(0.9, 0.4, 0.2) */;
+uniform vec4 bgcolor; // = vec4(0.0, 0.0, 0.0, 1.0)
 vec4 transition (vec2 uv) {
   return mix(
-    getFromColor(uv) + vec4(progress*color, 1.0),
-    getToColor(uv) + vec4((1.0-progress)*color, 1.0),
+    getFromColor(uv) + vec4(progress*color, bgcolor.a),
+    getToColor(uv) + vec4((1.0-progress)*color, bgcolor.a),
     progress
   );
 }
