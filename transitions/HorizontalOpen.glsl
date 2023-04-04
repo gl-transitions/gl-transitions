347b00921e841c4009ca1f9b37b121b52083aa5c
martyniukigor <83081473+martyniukigor@users.noreply.github.com>
2023-04-04 10:35:30 +0300

    Create HorizontalOpen.glsl (#189)

---
+++ transitions/HorizontalOpen.glsl
@@ -0,0 +1,15 @@
+// Author: martiniti
+// License: MIT
+
+vec4 transition (vec2 uv) {
+  
+  float regress = 1.0 - progress;
+
+  float s = 2.0 - abs((uv.y - 0.5) / (regress - 1.0)) - 2.0 * regress;
+  
+  return mix(
+    getFromColor(uv),
+    getToColor(uv),
+    smoothstep(0.0, 0.5, s)
+  );
+}
